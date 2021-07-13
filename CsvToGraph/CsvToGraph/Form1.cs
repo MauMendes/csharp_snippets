13ee5bb535c8ebae4b199304523150ecc0943b52
Mauricio Mendes <mauricio.eletrica@gmail.com>
2021-07-14 11:28:32 +1200

    Added a List of a List to contain data

--- CsvToGraph/CsvToGraph/Form1.cs
+++ CsvToGraph/CsvToGraph/Form1.cs
@@ -9,6 +9,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using ScottPlot.Renderable;
+using ScottPlot.Plottable;
+
 namespace CsvToGraph
 {
     public partial class Form1 : Form
@@ -18,6 +21,7 @@ namespace CsvToGraph
             InitializeComponent();
         }
 
+
         private void button1_Click(object sender, EventArgs e)
         {
             openFileDialog1.InitialDirectory =
@@ -26,8 +30,10 @@ namespace CsvToGraph
             openFileDialog1.Multiselect = false;
             openFileDialog1.FileName = "";
 
-            List<double> data = new List<double>();
-            List<double> data2 = new List<double>();
+
+            List<string> Header = new List<string>();
+            List<List<double>> data = new List<List<double>>();
+
             char[] delimiters = { '\t' };
 
 
@@ -36,29 +42,74 @@ namespace CsvToGraph
                 string FileName = openFileDialog1.SafeFileName;
                 string DirectoryPath = Path.GetDirectoryName(WholePath);
 
-                MessageBox.Show(WholePath + " " + FileName + " " + DirectoryPath);
+                //MessageBox.Show(WholePath + " " + FileName + " " + DirectoryPath);
 
                 Stream stream = File.Open(WholePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
                 StreamReader FileStream = new StreamReader(stream);
-                string HeaderLine = FileStream.ReadLine();
-           
-                var columns = HeaderLine.Split(delimiters);
 
-                string Line;
-                while ((Line = FileStream.ReadLine()) != null)
+                string []Lines = File.ReadAllLines(WholePath);
+                string []HeaderLine = Lines[0].Split(delimiters);
+
+
+                
+                foreach (var item in HeaderLine)
                 {
-                    string[] DataColumns = FileStream.ReadLine().Split(delimiters);
-                    data.Add(Convert.ToDouble(DataColumns[0]));
-                    data2.Add(Convert.ToDouble(DataColumns[4]));
+                    if (item != "")
+                    {
+                        Header.Add(item);
+                        data.Add(new List<double>());
+                    }
                 }
 
+
+                int skip = 0;
+                foreach (var line in Lines)
+                {
+                    if (skip == 0)//skipHeader
+                    {
+                        skip = 1;
+                        continue;
+                    }
+                    else 
+                    { 
+                        var items = line.Split(delimiters);
+                        int index = 0;
+                        foreach (var item in items)
+                        {
+                            if (!string.IsNullOrEmpty(item))
+                            {
+                                if(index < data.Count)
+                                {
+                                    data[index].Add(Convert.ToDouble(item));
+                                    index++;
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
 
             var plt = formsPlot1.Plot;
 
-            plt.AddSignal(data.ToArray(), sampleRate: 48_000);
-            plt.AddSignal(data2.ToArray(), sampleRate: 48_000);
+            List<SignalPlot> lstAxis = new List<SignalPlot>();
+            
+
+            for (int i = 0; i < 4; i++)
+            {
+                var axis = plt.AddSignal(data[i].ToArray(), sampleRate: 48_000);
+                lstAxis.Add(axis);
+            }
+
+            lstAxis[0].YAxisIndex = 0;
+            lstAxis[1].YAxisIndex = 1;
+
+
+            plt.AddAxis(Edge.Left, 2);
+            plt.AddAxis(Edge.Left, 3);
+
+
+            //plt.AddSignal(data2.ToArray(), sampleRate: 48_000);
             plt.Title("One Million Points");
             plt.AxisAuto();
 
